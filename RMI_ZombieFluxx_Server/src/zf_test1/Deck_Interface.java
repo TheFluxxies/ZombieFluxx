2968d6252d8259916da68bdad29ede59b7176a50
HikariV <Vincent.nuis@gmail.com>
2017-06-20 12:36:36 +0200

    Deck changes

--- RMI_ZombieFluxx_Server/src/zf_test1/Deck_Interface.java
+++ RMI_ZombieFluxx_Server/src/zf_test1/Deck_Interface.java
@@ -5,7 +5,8 @@ import java.rmi.RemoteException;
 
 public interface Deck_Interface extends Remote {
 	void SayConnected() throws RemoteException;
+	void startUp() throws RemoteException;
 	void addCard() throws RemoteException;
-	void removeCard() throws RemoteException;
+	void removeCard(String Card) throws RemoteException;
 	void getCard() throws RemoteException;
 }
