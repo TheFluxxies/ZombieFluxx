a9d62a76c356e30272a29afdb95e7da20eed3ce5
HikariV <Vincent.nuis@gmail.com>
2017-06-21 11:57:13 +0200

    Hrrngh

--- RMI_ZombieFluxx_Server/src/zf_test1/Discard.java
+++ RMI_ZombieFluxx_Server/src/zf_test1/Discard.java
@@ -1,40 +1,11 @@
 package zf_test1;
 
+public class Discard implements Discard_Interface{
+	//List
+	
+	//Methode: Discard(Card card){
+	//List.add(card);
 
-import java.io.IOException;
-import java.rmi.RemoteException;
-
-public class Discard implements Deck_Interface{
-
-	@Override
-	public void SayConnected() throws RemoteException {
-		// TODO Auto-generated method stub
-		System.out.println("Discard Pile is Connected to Client");
-	}
-
-	@Override
-	public void addCard() throws RemoteException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void getCard() throws RemoteException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void startUp() throws RemoteException, IOException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void removeCard(String Card) throws RemoteException {
-		// TODO Auto-generated method stub
-		
-	}
-
-
+	//Methode: public void Clear(){
+	//Leeg halen
 }
