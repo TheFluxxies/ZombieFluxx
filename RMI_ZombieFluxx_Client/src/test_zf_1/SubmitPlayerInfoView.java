49c7fb4f1c0aa4ae6381ad84285cb100ad30a68f
HikariV <Vincent.nuis@gmail.com>
2017-06-21 16:09:02 +0200

    Belangrijkste is spelregels showen (in menu)

--- RMI_ZombieFluxx_Client/src/test_zf_1/SubmitPlayerInfoView.java
+++ RMI_ZombieFluxx_Client/src/test_zf_1/SubmitPlayerInfoView.java
@@ -31,13 +31,13 @@ import zf_test1.Player;
 public class SubmitPlayerInfoView extends Application{
 
 static ArrayList<Player> allplayers = new ArrayList<Player>();
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		launch(args);
 	}
 
-	private GameMenu gameMenu; 
+	private GameMenu gameMenu;
 	Pane pane;
 	InputStream is;
 	Image img;
@@ -48,77 +48,77 @@ static ArrayList<Player> allplayers = new ArrayList<Player>();
 		this.arg1 = arg1;
 		pane = new Pane(); // Make new Pane named pane.
 		pane.setPrefSize(breedte,hoogte); // Set window size to 800 by 600
-		
+
 		try {
 			is = Files.newInputStream(Paths.get("res/images/zombieBackground2.jpg"));
-			img = new Image(is); // Make a new Image named img and set background as image 
+			img = new Image(is); // Make a new Image named img and set background as image
 			is.close(); // close inputStream
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} // Import background image 
-		
+		} // Import background image
+
 		ImageView imgView = new ImageView(img); // make new ImageView named imgView
 		imgView.setFitWidth(breedte); // set image width
 		imgView.setFitHeight(hoogte); // set image Height
-		
+
 		gameMenu = new GameMenu(); // Make new gameMenu
-		
-		pane.getChildren().addAll(imgView, gameMenu); // set image en gamemenu to pane 
-		
-		Scene scene = new Scene(pane); // create scene 
-		
-		arg1.setScene(scene); 
+
+		pane.getChildren().addAll(imgView, gameMenu); // set image en gamemenu to pane
+
+		Scene scene = new Scene(pane); // create scene
+
+		arg1.setScene(scene);
 		arg1.show();
-		
-		
+
+
 	}
-	
-	private static class MenuButton extends StackPane{ 
+
+	private static class MenuButton extends StackPane{
 		private Text text;
-		
+
 		public MenuButton(String name){
-			// Text formate 
+			// Text formate
 			text = new Text(name);
 			text.getFont();
 			text.setFont(Font.font(20));
 			text.setFill(Color.WHITE);
-			
+
 			// menu button background.
 			Rectangle bg = new Rectangle(250, 30);
 			bg.setOpacity(0.5);
 			bg.setFill(Color.BLACK);
 			bg.setEffect(new GaussianBlur(3.5));
-			
-			// 
+
+			//
 			setAlignment(Pos.CENTER_LEFT);
 			setRotate(0.5);
 			getChildren().addAll(bg, text);
-			
-			// What happens when you go over the buttons with your mouse 
+
+			// What happens when you go over the buttons with your mouse
 			setOnMouseEntered(event -> {
 				bg.setTranslateX(0);
 				text.setTranslateX(0);
 				bg.setFill(Color.GREEN);
 				text.setFill(Color.WHITE);
 			});
-			
-			// What happens when your mouse leaves the button 
+
+			// What happens when your mouse leaves the button
 			setOnMouseExited(event -> {
 				bg.setTranslateX(0);
 				text.setTranslateX(0);
 				bg.setFill(Color.BLACK);
 				text.setFill(Color.WHITE);
 			});
-			
-			// What happens when you click on a button with your mouse 
+
+			// What happens when you click on a button with your mouse
 			DropShadow drop = new DropShadow(50, Color.RED);
 			drop.setInput(new Glow());
-			
+
 			setOnMousePressed(event -> setEffect(drop));
 			setOnMouseReleased(event -> setEffect(null));
-			
-			
+
+
 		}
 
 	}
@@ -134,14 +134,14 @@ static ArrayList<Player> allplayers = new ArrayList<Player>();
 	MenuButton Cancel = new MenuButton("Cancel");
 	private class GameMenu extends Parent {
 		public GameMenu() throws RemoteException {
-			
+
 			LobbyView lv = new LobbyView();
 			MenuView s = new MenuView();
 			l2.setTextFill(Color.RED);
 			l2.setVisible(false);
-			// All the different buttons and what they do onmouseclick 
-			
-			
+			// All the different buttons and what they do onmouseclick
+
+
 			Continue.setOnMouseClicked(event -> {
 				if (allplayers.size() == 4){
 					System.err.println("ERROR : MAX PLAYER REACHED");
@@ -151,9 +151,9 @@ static ArrayList<Player> allplayers = new ArrayList<Player>();
 					t.setVisible(false);
 					l1.setVisible(false);
 					t2.setVisible(false);
-					
+
 					}else if(t.getText() != "" && t2.getText() != "" && t.getText() != null && t2.getText() != null){
-					
+
 					Player x = new Player(t.getText());
 				    allplayers.add(x);
 				    try {
@@ -168,23 +168,23 @@ static ArrayList<Player> allplayers = new ArrayList<Player>();
 					l2.setVisible(true);
 					System.err.println("");
 				}
-				
+
 			});
-			
-			
+
+
 			Cancel.setOnMouseClicked(event -> {
-				
-				
+
+
 				try {
 					s.start(s.arg0);
 					arg1.close();
-				} catch (RemoteException e1) {
+				} catch (IOException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				
+
 			});
-			
+
 			t.setOnAction(e ->{
 				if (t.getText() != null){
 					Continue.setVisible(true);
@@ -198,17 +198,17 @@ static ArrayList<Player> allplayers = new ArrayList<Player>();
 			v2.getChildren().addAll(l1,t2);
 			v.setSpacing(10);
 			v2.setSpacing(10);
-			
-			// Name and textfield 
+
+			// Name and textfield
 			v.setTranslateX(250);
 			v.setTranslateY(250);
 			// Ip and textfield
 			v2.setTranslateX(250);
 			v2.setTranslateY(300);
-			// buttons 
+			// buttons
 			h.setTranslateX(250);
 			h.setTranslateY(500);
-			
+
 			//
 			l2.setTranslateX(250);
 			l2.setTranslateX(150);
@@ -216,14 +216,14 @@ static ArrayList<Player> allplayers = new ArrayList<Player>();
 			Rectangle bg = new Rectangle(800, 600);
 			bg.setFill(Color.GRAY);
 			bg.setOpacity(0.4);
-			
+
 			getChildren().addAll(bg,h,v,v2);
-			
-			
-			
+
+
+
 		}
-		
-	
+
+
 }
 
 }
--- RMI_ZombieFluxx_Server/src/zf_test1/Action.java
+++ RMI_ZombieFluxx_Server/src/zf_test1/Action.java
@@ -1,5 +1,11 @@
 package zf_test1;
 
 public class Action {
-
+	Card card;
+	
+	void UseCard(Card card){
+		this.card = card;
+	}
 }
+
+
