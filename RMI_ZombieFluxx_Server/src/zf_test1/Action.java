e3177f19e92076928ac9fba376fab9c04814a369
Bruno de Badts <bdebadts@outlook.com>
2017-06-22 15:08:20 +0100

    ocmmit

--- RMI_ZombieFluxx_Server/src/zf_test1/Action.java
+++ RMI_ZombieFluxx_Server/src/zf_test1/Action.java
@@ -1,5 +1,13 @@
-package zf_test1;
+import java.util.ArrayList;
+
+import zf_test1.Card;
 
 public class Action {
 
+	void ...and_Stay_Dead(){
+		if(player == active){
+			
+		}
+	}
+	
 }
--- RMI_ZombieFluxx_Server/src/zf_test1/Newrule.java
+++ RMI_ZombieFluxx_Server/src/zf_test1/Newrule.java
@@ -1,5 +1,77 @@
-package zf_test1;
+import java.util.ArrayList;
+
+import zf_test1.Card;
 
 public class Newrule {
 
+	//hand limit
+	ArrayList<Card> hand = new ArrayList<Card>();
+	void hand_limit_0(){
+		if(player == active){
+			//doe niks
+		}
+		else {
+			hand.clear();
+		}
+	}
+	
+	void hand_limit_1(){
+		if(player == active){
+			//doe niks
+		}
+		else if(hand.size() > 1 ){	
+			//speler kiest 1 kaart om te behouden
+		}
+	}
+	
+	void hand_limit_2(){
+		if(player == active){
+			//doe niks
+		}
+		else if(hand.size()> 2 ){	
+			//speler kiest 2 kaarten om te behouden
+		}
+	}
+	
+	void hand_limit_3(){
+		if(player == active){
+			//doe niks
+		}
+		else if(hand.size() > 3 ){	
+			//speler kiest 3 kaarten om te behouden
+		}
+	}
+	//draw cards
+	void draw_2(){
+		this.draw = 2;
+	}
+	
+	void draw_3(){
+		this.draw = 3;
+	}
+	
+	void draw_4(){
+		this.draw = 4;
+	}
+	
+	void draw_5(){
+		this.draw = 5;
+	}
+	
+	//play cards
+	void play_2(){
+		this.play = 2;
+	}
+	
+	void play_3(){
+		this.play = 3;
+	}
+	
+	void play_4(){
+		this.play = 4;
+	}
+	
+	void play_all(){
+		this.play = hand.size();
+	}
 }
